5153b19742104b41e70e6ce9e432efac6f9a626d
sapaev_n <snu5207824@icloud.com>
2023-03-27 19:53:18 +0500

    colors(responsive design)

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 'use strict'
 const btn = document.querySelector('button.btn');
-
 btn.addEventListener('click',changeBgColor);
-let index = 0
 function changeBgColor(){
-	const arr = ['orangered','red','blue','purple','green','yellow']
-
-	document.querySelector('body').style.background = arr[index]
-	index++
-	if(index > arr.length-1){
-		index = 0;
-	}
+	document.querySelector('body').style.background = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+	console.log(`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`)
 }
